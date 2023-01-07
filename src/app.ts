2e0919cc9721a3cae74685ddeb4348e642a76f03
Carlos Villacorta <carlos@lawnbuddy.com>
2023-01-07 01:33:10 -0500

    challenge first changes

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,81 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
+import {
+  createQueryValidationRule,
+  constraintDirectiveTypeDefs,
+} from "graphql-constraint-directive";
+import express, { request } from "express";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { Comment, Post } from "../src/models";
+import { PossibleTypeExtensionsRule } from "graphql";
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const schema = buildSchema(`
+const appTypeDefs = `
   type Query {
-    hello: String
+    posts: [Post]
   }
-`);
+
+  type Mutation {
+    newComment(comment:CommentInput) : Comment
+  }
+
+  input CommentInput {
+    title: String! @constraint(minLength: 10)
+  }
+
+  type Post {
+    title: String
+    body: String
+    comments: [Comment]
+  }
+
+  type Comment {
+    name: String
+    email: String
+    body: String
+  }
+`;
+
+type Post = {
+  title: string;
+  body: string;
+  comments: [Comment];
+};
+
+type Comment = {
+  name: string;
+  email: string;
+  body: string;
+};
+
+const schema = makeExecutableSchema({
+  typeDefs: [constraintDirectiveTypeDefs, appTypeDefs],
+});
 
 const root = {
-  hello: () => {
-    return 'Hello world!';
+  posts: async () => {
+    const posts = await Post.find().exec();
+    console.log(posts);
+
+    return posts;
   },
 };
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP(async (request, response, params) => ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+    validationRules: [
+      createQueryValidationRule({ variables: params?.variables }),
+    ],
+  }))
+);
 
 export { app };
